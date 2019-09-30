d88ead3a23963e184d11a1767d3ab076317e2cfb
Craig <craig.m.jarrett@gmail.com>
2019-09-30 00:29:33 -0400

    Update testHW04_CraigJarrett.py

--- testHW04_CraigJarrett.py
+++ testHW04_CraigJarrett.py
@@ -4,6 +4,7 @@ Created on Sep 24, 2019
 @author: CJ
 '''
 import unittest
+from unittest.mock import patch
 import requests
 class Test(unittest.TestCase):
     """Unit Test"""
@@ -11,15 +12,15 @@ class Test(unittest.TestCase):
     def setUp(cls):
         cls.ID = 'cjarrett1'
         cls.ID2 = 'INVALID_USER'
-    def test_validuser(self):
+    @patch('requests.get')
+    def test_validuser(self, mock_get):
         """Check if user is a valid user"""
-        api = 'https://api.github.com/users/'+self.ID+'/repos'
-        r_1 = requests.get(api)
-        self.assertEqual(r_1.status_code, 200)
-    def test_invaliduser(self):
+        mock_get.return_value.status_code = 200
+        self.assertEqual(mock_get.return_value.status_code, 200)
+    @patch('requests.get')
+    def test_invaliduser(self, mock_get):
         """Check if user is not valid. Status code should return error."""
-        api = 'https://api.github.com/users/'+self.ID2+'/repos'
-        r_1 = requests.get(api)
-        self.assertEqual(r_1.status_code, 404)
+        mock_get.return_value.status_code = 404
+        self.assertEqual(mock_get.return_value.status_code, 404)
 if __name__ == "__main__":
     unittest.main()
